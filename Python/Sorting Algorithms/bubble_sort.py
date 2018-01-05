308a7f183de7a9921ba9f9974f91fd7a20b09400
Shilpa Jain <shilpa.jain2014@outlook.com>
2018-01-05 19:58:29 +0530

    cleaned code

--- Python/Sorting Algorithms/bubble_sort.py
+++ Python/Sorting Algorithms/bubble_sort.py
@@ -5,13 +5,11 @@ def bubble_sort(alist):
    #Traverse through all the array elements.
     for i in range(0, len(alist)):
         for j in range(0,len(alist)-i-1):
-            #print(j);
             if(alist[j] > alist[j + 1]):
                 temp = alist[j];
                 alist[j] = alist[j+1];
                 alist[j+1] = temp;
 
 alist = [54,26,93,17,77,31,44,55,20];
-print(len(alist));
 bubble_sort(alist);
 print(alist);
