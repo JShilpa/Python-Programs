4554e449ca55debea959d089a04f7e3dc0903479
Shilpa Jain <shilpa.jain2014@outlook.com>
2018-01-27 23:03:37 +0530

    adding search and remove menthod in linked list

--- Data Structures/Linked List/singlelinkedlist.py
+++ Data Structures/Linked List/singlelinkedlist.py
@@ -37,14 +37,56 @@ class LinkedList:
         return count
 
     def print_element(self):
+        """Prints the elements"""
         current = self.head;
         while current is not None:
             print(current.get_data())
             current = current.get_next()
 
+    def search_element(self, val):
+        """Search for element in the list. If found, return True. It not found, return False"""
+        current = self.head
+        found = False
+        while current is not None and not found:
+            if current.get_data() is val:
+                found = True
+            else:
+                current = current.get_next()
+        return found
+
+    def remove_element(self, val):
+        """Remove element from the list. If element not found, raise ValueError"""
+        found = False
+        previous = None
+        current = self.head
+        while current is not None and not found:
+                if current.get_data() is val:
+                    #previous.set_next(current.get_next())
+                    found = True
+                else:
+                   previous = current
+                   current = current.get_next()
+        if found:
+            if previous is None:
+                self.head = current.get_next()
+            else:
+                previous.set_next(current.get_next())
+        else:
+            raise ValueError
+            print("Value not found")
+                   #print(current)
+        #return removed
+
+
+
+
 if __name__ == '__main__':
         myList = LinkedList()
+        myList.add_element('S')
         myList.add_element('L')
         myList.add_element('I')
         print(myList.size())
+        myList.print_element()
+        print(myList.search_element('I'))
+        print(myList.remove_element('I'))
         myList.print_element()
